3e211480d34fb826eb4056f3a6b49e14c5eb5dd8
PXLPERS\20003575 <custersnele@gmail.com>
2018-05-08 11:52:23 +0200

    format EntityManagerUtil

--- src/main/java/be/pxl/ja2/beers/dao/EntityManagerUtil.java
+++ src/main/java/be/pxl/ja2/beers/dao/EntityManagerUtil.java
@@ -8,20 +8,23 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
-public class EntityManagerUtil implements ServletContextListener{
-    private static EntityManagerFactory emf;
-    @Override
-    public void contextInitialized(ServletContextEvent event) {
-        emf = Persistence.createEntityManagerFactory("studentDB");
-    }
-    @Override
-    public void contextDestroyed(ServletContextEvent event) {
-        emf.close();
-    }
-    public static EntityManager createEntityManager() {
-        if (emf == null) {
-            throw new IllegalStateException("Context is not initialized yet.");
-        }
-        return emf.createEntityManager();
-    }
+public class EntityManagerUtil implements ServletContextListener {
+	private static EntityManagerFactory emf;
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		emf = Persistence.createEntityManagerFactory("studentDB");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		emf.close();
+	}
+
+	public static EntityManager createEntityManager() {
+		if (emf == null) {
+			throw new IllegalStateException("Context is not initialized yet.");
+		}
+		return emf.createEntityManager();
+	}
 }
