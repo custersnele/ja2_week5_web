1eada29f86c808bb6380791c47b8e2b7012c4e1c
PXLPERS\20003575 <custersnele@gmail.com>
2019-05-09 21:52:37 +0200

    update code

--- src/main/java/be/pxl/ja2/dao/BrewerDao.java
+++ src/main/java/be/pxl/ja2/dao/BrewerDao.java
@@ -2,20 +2,22 @@ package be.pxl.ja2.dao;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import be.pxl.ja2.data.Brewer;
 
 public class BrewerDao {
-	private EntityManager em;
-	
+	private EntityManager entityManager;
+
 	public BrewerDao(EntityManager entityManager) {
-		this.em = entityManager;
+		this.entityManager = entityManager;
 	}
-	
+
 	public List<Brewer> findByCity(String city) {
-		TypedQuery<Brewer> findByCity = em.createNamedQuery("findByCity", Brewer.class);
+		TypedQuery<Brewer> findByCity = entityManager.createNamedQuery("findByCity", Brewer.class);
 		System.out.println("findByCity" + findByCity);
 		findByCity.setParameter("city", city);
 		
--- src/main/java/be/pxl/ja2/dao/EntityManagerUtil.java
+++ src/main/java/be/pxl/ja2/dao/EntityManagerUtil.java
@@ -6,25 +6,43 @@ import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.util.Enumeration;
 
 @WebListener
 public class EntityManagerUtil implements ServletContextListener {
-	private static EntityManagerFactory emf;
+    private static EntityManagerFactory emf;
 
-	@Override
-	public void contextInitialized(ServletContextEvent event) {
-		emf = Persistence.createEntityManagerFactory("studentDB");
-	}
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        emf = Persistence.createEntityManagerFactory("studentDB");
+    }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		emf.close();
-	}
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        emf.close();
+        // https://www.ralph-schuster.eu/2014/07/09/solution-to-tomcat-cant-stop-an-abandoned-connection-cleanup-thread/
+        try {
+            com.mysql.jdbc.AbandonedConnectionCleanupThread.shutdown();
+        } catch (Throwable t) {
+            // TODO log exception
+        }
+        // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory leaks
+        Enumeration<Driver> drivers = java.sql.DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            java.sql.Driver driver = drivers.nextElement();
+            try {
+                java.sql.DriverManager.deregisterDriver(driver);
+            } catch (Throwable t) {
+                // TODO log exception
+            }
+        }
+    }
 
-	public static EntityManager createEntityManager() {
-		if (emf == null) {
-			throw new IllegalStateException("Context is not initialized yet.");
-		}
-		return emf.createEntityManager();
-	}
+    public static EntityManager createEntityManager() {
+        if (emf == null) {
+            throw new IllegalStateException("Context is not initialized yet.");
+        }
+        return emf.createEntityManager();
+    }
 }
